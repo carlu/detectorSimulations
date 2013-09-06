c986a56aaaf15010f50467982e3139fdf6656b67
Carl Unsworth <carlunsworthuk@gmail.com>
2013-09-05 18:20:48 -0700

    - Updates to validation sort code. - Added code to interpret "Collection" string and decide if energy was in shield or HPGe - Added spectra for shields (but I just looked and they were empty, will fix this soon).

--- validation/ValidationSort.cc
+++ validation/ValidationSort.cc
@@ -1,7 +1,8 @@
 // ValidationSort.cc - 5 Sept 2013 - C Unsworth
 // To Compile:
-// g++ ValidationSort.cc --std=c++0x -o ValidationSort -O2 `root-config --cflags --libs`
-//
+// g++ ValidationSort.cc --std=c++0x -o ValidationSort -O2 -Wall `root-config --cflags --libs`
+// To Run:
+// ./ValidationSort [filename]
 // Quick sort to process TIGRESS/GRIFFIN GEANT4 simulation output and produce spectra
 // Which can be compared with experiment.
 
@@ -21,12 +22,14 @@ using namespace std;
 #define CRYSTALS 64   // Total number of HPGe crystals
 #define CLOVERS  16   // Total number of clovers in array
 #define COLOURS   4   // Number of crystals per clover (Blue,Green,Red,White)
+#define MIN_EN   0.000001  // Minimum energy to be incremented to spectra
 
 // File pointers:
 static TFile *outfile = 0;
 
 // Spectra pointers here:
 static TH1F *CrystalEnergy[CRYSTALS];
+static TH1F *ShieldEnergy[CRYSTALS]; // For now only a sum of shield energy, no individual shield elements.
 
 
 int main(int argc, char **argv){
@@ -41,6 +44,7 @@ int main(int argc, char **argv){
    char Process[24], Collection[64];
    // For holding data items for the whole event....   
    float CrysEn[CRYSTALS];
+   float ShieldEn[CRYSTALS];
    
    //  Open the input file
    ifstream infile;
@@ -64,6 +68,9 @@ int main(int argc, char **argv){
       sprintf(name,"Crystal%d",i); 
       sprintf(title,"Crystal %d Energy (keV)",i); 
       CrystalEnergy[i] = new TH1F(name,title,8192,0,2048);
+      sprintf(name,"Shield%d",i); 
+      sprintf(title,"Shield %d Energy (keV)",i);      
+      ShieldEnergy[i] = new TH1F(name,title,8192,0,2048);
    }
    
    // Start main loop....
@@ -75,9 +82,11 @@ int main(int argc, char **argv){
       
       if(str.substr(0,4)=="Hits") {  // If this is a new event then fill spectra and reset event data
          for(i=0;i<CRYSTALS;i++) {
-            if(CrysEn[i] > 0.0) {CrystalEnergy[i]->Fill(CrysEn[i]);}  
+            if(CrysEn[i] > MIN_EN) {CrystalEnergy[i]->Fill(CrysEn[i]);}
+            if(ShieldEn[i] > MIN_EN) {ShieldEnergy[i]->Fill(ShieldEn[i]);}  
          }   
          memset(CrysEn,0.0,(CRYSTALS*sizeof(float)));
+         memset(ShieldEn,0.0,(CRYSTALS*sizeof(float)));
          continue;
       }
       else {  // This is an interaction to be added to event record...
@@ -86,8 +95,15 @@ int main(int argc, char **argv){
          
          crystal = ((DetN -1) * 4) + SegN -1; // Get crystal number from detector and segment
                      // note: "segment" in GEANT output means crystal in real world 
+         //printf("%s\n%s\n",Process, Collection);
+         if(strcmp(Collection,"CollectionGriffinForwardGe") || strcmp(Collection,"CollectionGriffinBackGe")) {
+            CrysEn[crystal] += EkeV; // sum energy in this crystal  
+         }
+         else{ 
+            ShieldEn[crystal] += EkeV;
+         }
          
-         CrysEn[crystal] += EkeV; // sum energy in this crystal
+         
                      // note: At the moment this is summing HPGe and BGO energy for each colour, need to parse
                      // "Collection" string to fix this        
       } // close secttion for processing interactions   
@@ -96,6 +112,7 @@ int main(int argc, char **argv){
    // Now write the spectra....  (I think I can do these all at once but not sure how just yet.
    for(i=0;i<CRYSTALS;i++) {
       CrystalEnergy[i]->Write();
+      ShieldEnergy[i]->Write();
    }
    
    outfile->Close();
